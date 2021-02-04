1931b48347fe07edf33c08f0daf724b826fdbe3b
Rehaan Mazid <38107493+Rehaan1@users.noreply.github.com>
2021-02-04 22:56:32 +0530

    Backup 101.  Pacman moving from node to node smoothly

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour 
+{
+
+	public Node[] neighbours;
+	public Vector2[] validDirections;
+
+	// Use this for initialization
+	void Start () 
+	{
+		validDirections = new Vector2[neighbours.Length];
+
+		for(int i=0;i<neighbours.Length;i++)
+		{
+			Node  neighbour = neighbours[i];
+			Vector2 tempVector = (Vector2)(neighbour.transform.localPosition - transform.localPosition);
+
+			validDirections[i] = tempVector.normalized;
+		}
+	}
+	
+}
---
+++ Assets/Scripts/PacMan.cs
@@ -0,0 +1,199 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacMan : MonoBehaviour
+{
+    public float speed= 4.0f;
+
+    public Sprite idleSprite;
+    private Vector2 direction = Vector2.zero;
+    private Vector2 nextDirection;
+    private Node currentNode, previousNode, targetNode;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Node node = GetNodeAtPosition(transform.localPosition);
+        if(node != null)
+        {
+            currentNode = node;
+            Debug.Log(currentNode);
+        }
+
+        direction = Vector2.left;
+        ChangePosition(direction);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckInput();
+        Move();
+        UpdateOrientation();
+        UpdateAnimationState();
+    }
+
+    void CheckInput()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            ChangePosition(Vector2.left);
+        }
+        else if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            ChangePosition(Vector2.right);
+        }
+        else if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            ChangePosition(Vector2.up);
+        }
+        else if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            ChangePosition(Vector2.down);
+        }
+    }
+
+    void ChangePosition(Vector2 d)
+    {
+        if(d != direction)
+        {
+            nextDirection = d;
+        }
+
+        if(currentNode  != null)
+        {
+            Node movetoNode = CanMove(d);
+            if(movetoNode != null)
+            {
+                direction = d;
+                targetNode = movetoNode;
+                previousNode = currentNode;
+                currentNode = null;
+            }
+        }
+    }
+
+    void Move()
+    {
+        if(targetNode != currentNode && targetNode != null)
+        {
+            if(OverShotTarget())
+            {
+                currentNode = targetNode;
+                transform.localPosition = currentNode.transform.position;
+                
+                Node moveToNode = CanMove(nextDirection);
+                if(moveToNode !=null)
+                {
+                    direction = nextDirection;
+                }
+                if(moveToNode == null)
+                {
+                    moveToNode = CanMove(direction);
+                }
+                if(moveToNode != null)
+                {
+                    targetNode = moveToNode;
+                    previousNode = currentNode;
+                    currentNode = null;
+                }
+                else
+                {
+                    direction = Vector2.zero;
+                }
+            }
+            else
+            {
+                transform.localPosition += (Vector3) (direction * speed) * Time.deltaTime;        
+            }
+        }
+       
+    }
+
+    void MoveToNode(Vector2 d)
+    {
+        Node moveToNode = CanMove(d);
+        if(moveToNode!=null)
+        {
+            transform.localPosition  =  moveToNode.transform.position;
+            currentNode = moveToNode;
+        }
+    }
+
+    void  UpdateOrientation()
+    {
+        if(direction == Vector2.left)
+        {
+            transform.localScale = new Vector3(-1,1,1);
+            transform.localRotation = Quaternion.Euler(0,0,0);
+        }
+        else if(direction == Vector2.right)
+        {
+            transform.localScale = new Vector3(1,1,1);
+            transform.localRotation = Quaternion.Euler(0,0,0);
+        }
+        else if(direction == Vector2.up)
+        {
+            transform.localScale = new Vector3(1,1,1);
+            transform.localRotation = Quaternion.Euler(0,0,90);
+        }
+        else if(direction == Vector2.down)
+        {
+            transform.localScale = new Vector3(1,1,1);
+            transform.localRotation = Quaternion.Euler(0,0,270);
+        }
+    }
+
+    void UpdateAnimationState()
+    {
+        if(direction == Vector2.zero)
+        {
+            GetComponent<Animator>().enabled = false;
+            GetComponent<SpriteRenderer>().sprite = idleSprite;
+        }
+        else
+        {
+            GetComponent<Animator>().enabled = true;
+        }
+    }
+
+    Node CanMove (Vector2 d)
+    {
+        Node moveToNode = null;
+        for(int i = 0; i < currentNode.neighbours.Length;i++)
+        {
+            if(currentNode.validDirections[i] == d)
+            {
+                moveToNode = currentNode.neighbours[i];
+                break;
+            }
+        }
+        return moveToNode;
+    }
+
+    Node GetNodeAtPosition(Vector2 pos)
+    {
+        GameObject tile = GameObject.Find("Game").GetComponent<GameBoard>().board[(int)pos.x,(int)pos.y];
+        
+        if(tile!=null)
+        {
+            return tile.GetComponent<Node>();
+        }
+        return null;
+    }
+
+    bool OverShotTarget()
+    {
+        float nodeToTarget = LengthFromNode(targetNode.transform.position);
+        float nodeToSelf = LengthFromNode(transform.localPosition);
+        
+        return nodeToSelf > nodeToTarget; 
+    }
+
+    float LengthFromNode(Vector2 targetPosition)
+    {
+        Vector2 vec = targetPosition - (Vector2)previousNode.transform.position;
+        return vec.sqrMagnitude;
+    }
+}
