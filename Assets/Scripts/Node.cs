27cc1aa3998a6f78ae056299631c5bdc03483ffb
Rehaan Mazid <38107493+Rehaan1@users.noreply.github.com>
2021-02-07 14:58:02 +0530

    fix: Pacman Not going up fixed
    Fixed by removing auto-detection of neighbour node detection and making it manual

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -11,15 +11,17 @@ public class Node : MonoBehaviour
 	// Use this for initialization
 	void Start () 
 	{
-		validDirections = new Vector2[neighbours.Length];
+		//Commented for BugFix
+		
+		//validDirections = new Vector2[neighbours.Length];
 
-		for(int i=0;i<neighbours.Length;i++)
-		{
-			Node  neighbour = neighbours[i];
-			Vector2 tempVector = (Vector2)(neighbour.transform.localPosition - transform.localPosition);
+		//for(int i=0;i<neighbours.Length;i++)
+		//{
+		//	Node  neighbour = neighbours[i];
+		//	Vector2 tempVector = (Vector2)(neighbour.transform.localPosition - transform.localPosition);
 
-			validDirections[i] = tempVector.normalized;
-		}
+		//	validDirections[i] = tempVector.normalized;
+		//}
 	}
 	
 }
